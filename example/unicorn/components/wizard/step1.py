d70b458379b4df4b55caf0051313cd91585e8737
Roman Imankulov <roman.imankulov@gmail.com>
2023-10-02 16:31:50 +0100

    Update wizard objects

--- example/unicorn/components/wizard/step1.py
+++ example/unicorn/components/wizard/step1.py
@@ -1,12 +1,12 @@
 from django_unicorn.components import UnicornView
 
 class Step1View(UnicornView):
-    name = ""
-    email = ""
+    name = "Roman"
+    email = "roman@example.com"
 
     def mount(self):
-        self.name = "Test"
-        self.email = ""
+        self.name = "Roman"
+        self.email = "roman@example.com"
 
     def next(self):
         self.parent.step += 1
